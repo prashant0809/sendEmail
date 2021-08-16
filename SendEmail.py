e9731fb214faef46fba7e0f321aae2894072d7b4
Prashant Shastri <pshastri1997@gmail.com>
2021-08-16 17:10:38 +0530

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("prashantshastrikcsitglobal@gmail.com","Data@30071997")
-server.sendmail(from_addr="prashantshastrikcsitglobal@gmail.com",to_addrs="parth.mundhwa@kcsitglobal.com",msg="hello")
+server.login("prashantshastri@gmail.com","Data@300password")
+server.sendmail(from_addr="prashantshastri@gmail.com",to_addrs="parth.mundhwa.com",msg="hello")
 server.quit()
 
 
